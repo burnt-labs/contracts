06c5338d814785b644fd27b00bf9e9c53f297a55
Ash <ash@burnt.com>
2023-10-02 23:11:13 +0300

    disable random

--- account/src/lib.rs
+++ account/src/lib.rs
@@ -11,3 +11,14 @@ pub mod state;
 
 pub const CONTRACT_NAME: &str = "account";
 pub const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+// the random function must be disabled in cosmwasm
+use core::num::NonZeroU32;
+use getrandom::Error;
+
+pub fn always_fail(_buf: &mut [u8]) -> Result<(), Error> {
+    let code = NonZeroU32::new(Error::CUSTOM_START).unwrap();
+    Err(Error::from(code))
+}
+use getrandom::register_custom_getrandom;
+register_custom_getrandom!(always_fail);
