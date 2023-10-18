5ed8c90e98a4211aa41192d70979661db3cad540
Ash <ash@burnt.com>
2023-10-18 10:55:11 -0700

    tests covered in integration

--- account/src/auth/jwt.rs
+++ account/src/auth/jwt.rs
@@ -111,36 +111,3 @@ pub fn verify(
         })
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::auth::Authenticator;
-
-    #[test]
-    fn test_validate_token() {
-        let encoded_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6Imp3ay10ZXN0LWI5OGFjMTExLTg1MTUtNGY0OS05MDU2LTdmM2E5NzJmNzU4MSIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsicHJvamVjdC10ZXN0LTE4NWU5YTlmLThiYWItNDJmMi1hOTI0LTk1M2E1OWU4ZmY5NCJdLCJleHAiOjE2OTU4NDgxNjksImh0dHBzOi8vc3R5dGNoLmNvbS9zZXNzaW9uIjp7ImlkIjoic2Vzc2lvbi10ZXN0LTk1MDUyMzJkLTczNjUtNDExZC1hYzBlLTc3MWM2YWY2MmU3NCIsInN0YXJ0ZWRfYXQiOiIyMDIzLTA5LTI3VDIwOjUxOjA5WiIsImxhc3RfYWNjZXNzZWRfYXQiOiIyMDIzLTA5LTI3VDIwOjUxOjA5WiIsImV4cGlyZXNfYXQiOiIyMDIzLTEwLTI3VDIwOjUxOjA5WiIsImF0dHJpYnV0ZXMiOnsidXNlcl9hZ2VudCI6IiIsImlwX2FkZHJlc3MiOiIifSwiYXV0aGVudGljYXRpb25fZmFjdG9ycyI6W3sidHlwZSI6Im90cCIsImRlbGl2ZXJ5X21ldGhvZCI6ImVtYWlsIiwibGFzdF9hdXRoZW50aWNhdGVkX2F0IjoiMjAyMy0wOS0yN1QyMDo1MTowOVoiLCJlbWFpbF9mYWN0b3IiOnsiZW1haWxfaWQiOiJlbWFpbC10ZXN0LWY1ODU1MDU1LTc2OWMtNGVhMC04YzZhLTFmMTUyNDgzNWJmMiIsImVtYWlsX2FkZHJlc3MiOiJmaWxhbWVudCsxNjk1ODQ3ODUyMDY1QGJ1cm50LmNvbSJ9fV19LCJpYXQiOjE2OTU4NDc4NjksImlzcyI6InN0eXRjaC5jb20vcHJvamVjdC10ZXN0LTE4NWU5YTlmLThiYWItNDJmMi1hOTI0LTk1M2E1OWU4ZmY5NCIsIm5iZiI6MTY5NTg0Nzg2OSwic3ViIjoidXNlci10ZXN0LWY1MmRkZWQyLWM5M2EtNGVmZC05MTY5LTU3N2ZiZGY2Y2I4NiIsInRyYW5zYWN0aW9uX2hhc2giOiIweDEyMzQ1Njc4OTBfMyJ9.SkoLgjViIkP2kVcgz4fqA1CEWKbkN40behhs_ph-uCIAWNakCC_6FEfvcYgxBp1idk2IPRRZir-QAK_XV8ov_RHZuCqc8qd_bzlAwXV7cElHDf8oQxs44kA_P81QExoCABa3_ZzJ8KUNwzY0NbFxI3oJKDOYGxapi5aY5xsuGO3wUJX4PlsJ5xQhn254THgxpstqXEj56K1bcuDw_y-TQiNnP9R3vLZfGWj6BkZjB8dfgMp6FMRq_tBmo4l37pVHIA8v3-tlYSNoV7Z1P2uLsuxrM3_eh5zJ48vogwiOYAC2Ih90Pp2mF0leKOEXC4feTl_2oPIvGdXqbxgAmGKjpA";
-        let encoded_hash = "0x1234567890_3";
-        let hash_bytes = encoded_hash.as_bytes().to_vec();
-
-        let verification = verify(
-            &Timestamp::from_seconds(1695847870),
-            &hash_bytes,
-            encoded_token.as_bytes(),
-            &"project-test-185e9a9f-8bab-42f2-a924-953a59e8ff94".to_string(),
-            &"user-test-f52dded2-c93a-4efd-9169-577fbdf6cb86".to_string(),
-        );
-        assert!(verification.unwrap());
-    }
-
-    #[test]
-    fn test_dump_authenticator() {
-        let authenticator = Authenticator::Jwt {
-            aud: "project-test-185e9a9f-8bab-42f2-a924-953a59e8ff94".to_string(),
-            sub: "user-test-f52dded2-c93a-4efd-9169-577fbdf6cb86".to_string(),
-        };
-        let serialized = serde_json::to_string(&authenticator).unwrap();
-
-        println!("authenticator: {}", serialized);
-    }
-}
