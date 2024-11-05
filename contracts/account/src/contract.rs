822b69de131f58d6fcb1ad24644da9dc73fab83e
Ash <ash@burnt.com>
2024-11-05 18:47:58 +0700

    remove import

--- contracts/account/src/contract.rs
+++ contracts/account/src/contract.rs
@@ -1,4 +1,4 @@
-pub use absacc::AccountSudoMsg;
+use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
 
 use crate::error::ContractError;
@@ -20,6 +20,45 @@ pub fn instantiate(
 ) -> ContractResult<Response> {
     cw2::set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
     execute::init(deps, env, &mut msg.authenticator.clone())
+}
+
+#[cw_serde]
+pub struct Any {
+    pub type_url: String,
+    pub value: Binary,
+}
+
+/// Any contract must implement this sudo message (both variants) in order to
+/// qualify as an abstract account.
+#[cw_serde]
+pub enum AccountSudoMsg {
+    /// Called by the AnteHandler's BeforeTxDecorator before a tx is executed.
+    BeforeTx {
+        /// Messages the tx contains
+        msgs: Vec<Any>,
+
+        /// The tx serialized into binary format.
+        ///
+        /// If the tx authentication requires a signature, this is the bytes to
+        /// be signed.
+        tx_bytes: Binary,
+
+        /// The credential to prove this tx is authenticated.
+        ///
+        /// This is taken from the tx's "signature" field, but in the case of
+        /// AbstractAccounts, this is not necessarily a cryptographic signature.
+        /// The contract is free to interpret this as any data type.
+        cred_bytes: Option<Binary>,
+
+        /// Whether the tx is being run in the simulation mode.
+        simulate: bool,
+    },
+
+    /// Called by the PostHandler's AfterTxDecorator after the tx is executed.
+    AfterTx {
+        /// Whether the tx is being run in the simulation mode.
+        simulate: bool,
+    },
 }
 
 #[cfg_attr(not(feature = "library"), cosmwasm_std::entry_point)]
