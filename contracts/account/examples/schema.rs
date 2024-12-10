bc57c2bd62cd46208ecdb89eef2c561fc8a757d2
adairrr <32375605+adairrr@users.noreply.github.com>
2024-12-10 15:33:51 -0500

    Add schema generation scripts

---
+++ contracts/account/examples/schema.rs
@@ -0,0 +1,11 @@
+use account::msg::*;
+use cosmwasm_schema::write_api;
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        query: QueryMsg,
+        execute: ExecuteMsg,
+        migrate: MigrateMsg
+    };
+}
