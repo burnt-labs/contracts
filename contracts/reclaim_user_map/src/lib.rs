28fb08f375665b94fe19ac0b489c42bb0204a658
Burnt Val <val@burnt.com>
2025-06-16 18:05:10 -0400

    getRandom disable

--- contracts/reclaim_user_map/src/lib.rs
+++ contracts/reclaim_user_map/src/lib.rs
@@ -5,3 +5,14 @@ pub mod contract;
 mod error;
 pub mod msg;
 mod state;
+
+// the random function must be disabled in cosmwasm
+use core::num::NonZeroU32;
+use getrandom::Error;
+
+pub fn always_fail(_buf: &mut [u8]) -> Result<(), Error> {
+    let code = NonZeroU32::new(Error::CUSTOM_START).unwrap();
+    Err(Error::from(code))
+}
+use getrandom::register_custom_getrandom;
+register_custom_getrandom!(always_fail);
