bcf07372ccf2d7a15742bb7590b427401dcb2ba1
Ash <ash@burnt.com>
2026-03-12 17:03:51 -0500

    v3 ios app attest

--- libraries/ios-app-attest/src/lib.rs
+++ libraries/ios-app-attest/src/lib.rs
@@ -29,11 +29,11 @@ pub fn verify_attestation(app_id: String, key_id: String, challenge: Binary, cbo
     let attestation_result: Result<Attestation, _> = from_reader(cursor);
     let attestation = match attestation_result {
         Ok(attestation) => attestation,
-        Err(err) => return Err(StdError::generic_err(err.to_string())),
+        Err(err) => return Err(StdError::msg(err.to_string())),
     };
 
     match attestation.verify(challenge.to_base64().as_str(), app_id.as_str(), key_id.as_str(), timestamp, dev_env) {
         Ok(_) => Ok(()),
-        Err(err) => Err(StdError::generic_err(err.to_string())),
+        Err(err) => Err(StdError::msg(err.to_string())),
     }
 }
